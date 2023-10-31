3c4970b038e4707824a19503f01d1ac7b35dc581
Uchi <a26689@svalero.com>
2023-10-31 21:25:08 +0100

    kms

--- MoviesApp/app/src/main/java/com/example/moviesapp/IntroActivity.java
+++ MoviesApp/app/src/main/java/com/example/moviesapp/IntroActivity.java
@@ -2,7 +2,10 @@ package com.example.moviesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class IntroActivity extends AppCompatActivity {
     @Override
--- MoviesApp/app/src/main/java/com/example/moviesapp/LoginActivity.java
+++ MoviesApp/app/src/main/java/com/example/moviesapp/LoginActivity.java
@@ -2,7 +2,11 @@ package com.example.moviesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity{
     private EditText userEdt, passEdt;
@@ -19,13 +23,13 @@ public class LoginActivity extends AppCompatActivity{
     private void initView() {
         userEdt=findViewById(R.id.editTextText);
         passEdt=findViewById(R.id.editTextPassword);
-        loginBtn findViewById(R.id.loginBtn);
+        loginBtn=findViewById(R.id.loginBtn);
 
         loginBtn.setOnClickListener(v -> {
             if(userEdt.getText().toString().isEmpty() || passEdt.getText().toString().isEmpty()){
                 Toast.makeText(LoginActivity.this, "Please Fill your user and password", Toast.LENGTH_SHORT).show();
             }else if(userEdt.getText().toString().equals("test") && passEdt.getText().toString().equals("test")){
-                startActivity(new Intent (LoginActivity.this, MainActivity.class));
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
             }else{
                 Toast.makeText(LoginActivity.this, "Your user and password is not correct", Toast.LENGTH_SHORT).show();
             }
