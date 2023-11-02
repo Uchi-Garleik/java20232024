fb46e9ac123147908884888dc2c162c378528391
Uchi (Manel) <zekrom.uchi@gmail.com>
2023-11-02 12:36:53 +0100

    updated things and fixed things

--- www/vistas/V_Usuarios_Listado.php
+++ www/vistas/V_Usuarios_Listado.php
@@ -3,20 +3,32 @@
 
     echo '<table>';
     echo '<tr>';
+    echo '<th>Nombre</th>';
     echo '<th>Apellido 1</th>';
     echo '<th>Apellido 2</th>';
-    echo '<th>Nombre</th>';
     echo '<th>Email</th>';
     echo '<th>Movil</th>';
+    echo '<th>Sexo</th>';
+    echo '<th>Activo</th>';
     echo '</tr>';
+
+    function returnCosa($fila){
+        if ($fila['activo'] == 'S') {
+            return "activo";
+        }else{
+            return "inactivo";
+        }
+    }
 
     foreach($usuarios as $fila){
         echo '<tr>';
+        echo '<td>'.$fila['nombre'].'</td>';
         echo '<td>'.$fila['apellido_1'].'</td>';
         echo '<td>'.$fila['apellido_2'].'</td>';
-        echo '<td>'.$fila['nombre'].'</td>';
         echo '<td>'.$fila['mail'].'</td>';
         echo '<td>'.$fila['movil'].'</td>';
+        echo '<td>'.$fila['sexo'].'</td>';
+        echo '<td>'.returnCosa($fila).'</td>';
         echo '</tr>';
     }
 
